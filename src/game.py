2ee9d00ae4f6de99e06c645585edce0e24709200
Galatory <20934939+Galatory@users.noreply.github.com>
2020-03-10 15:46:18 +0100

    Rounded rectangles

--- src/game.py
+++ src/game.py
@@ -2,6 +2,7 @@ import math
 
 import pygame
 from pygame.locals import *
+from roundrects import aa_round_rect, round_rect
 
 from grid import Grid
 
@@ -21,15 +22,20 @@ class Game:
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
+        self.grid = Grid(4)
 
         # TODO: Prompt user for grid size (instead of hardcoded 4x4)
         self.display_size = 640
+        self.border_size = self.display_size / 80.0
+        self.cell_size = ((self.display_size - self.border_size * 2) - (
+                self.border_size * (self.grid.size - 1))) / self.grid.size
         self.screen = pygame.display.set_mode((self.display_size, self.display_size))
         self.screen.fill(pygame.Color("DARKGRAY"))
-        self.grid = Grid(4)
-        pygame.font.init()
+
+        # border_size + x*(border_size+cell_size)
 
         # TODO: Scale font size - necessary if we're gonna make nxn instead of 4x4
+        pygame.font.init()
         self.font = pygame.font.Font(r"res/m5x7.ttf", 72)
 
         # Filter events to enqueue - we only care if the user presses an arrow key
@@ -41,24 +47,29 @@ class Game:
         self.draw_grid(self.grid)
         pygame.display.flip()
 
+    def coord(self, index):
+        return self.border_size + index * (self.cell_size + self.border_size)
+
     def draw_grid(self, old_grid):
         # Defines cell w. spacing between cells
-        border_size = 5
-        cell_size = (self.display_size - border_size * (self.grid.size - 1)) / self.grid.size
-        cell_spacing = cell_size + border_size
 
         # Draw each cell in the grid
-        for x in range(self.grid.size):
-            for y in range(self.grid.size):
-                val = self.grid.grid[x][y]
-                rect = pygame.Rect(y * cell_spacing, x * cell_spacing, cell_size, cell_size)
-                pygame.draw.rect(self.screen, get_color(val), rect)
+        for y in range(self.grid.size):
+            for x in range(self.grid.size):
+                val = self.grid.grid[y][x]
+                rect = pygame.Rect(self.coord(x), self.coord(y), self.cell_size, self.cell_size)
+                rounded_rect = round_rect(self.screen, rect, get_color(val), rad=10)
+                # pygame.draw.rect(self.screen, get_color(val), rect)
 
                 if val != 0:
                     text = self.font.render(str(val), False, Color("BLACK"))
-                    text_width, text_height = self.font.size(str(val))
-                    _y = x * cell_spacing + (cell_size - text_height) / 2
-                    _x = y * cell_spacing + (cell_size - text_width) / 2
+                    _x = rect.centerx - text.get_rect().centerx
+                    _y = rect.centery - text.get_rect().centery
                     self.screen.blit(text, (_x, _y))
+
         pygame.display.flip()
         self.clock.tick(60)
+
+        # tekst starter ved 69
+        # 16 starter ved 55 ca
+        #
